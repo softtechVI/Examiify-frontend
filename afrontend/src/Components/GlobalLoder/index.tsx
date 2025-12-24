0e1da884088d18b35db779a54734d58f9daad2a4
ShubhamChouhan97 <shubhamsc9799@gmail.com>
2025-12-25 00:20:14 +0530

    comment out GlobalLoader component and its dependencies

--- afrontend/src/Components/GlobalLoder/index.tsx
+++ afrontend/src/Components/GlobalLoder/index.tsx
@@ -1,28 +1,28 @@
-import { Spin } from "antd";
-import useIsLoginStore from "../../store/IsLoginStore";
+// import { Spin } from "antd";
+// // import useIsLoginStore from "../../store/IsLoginStore";
 
-const GlobalLoader = () => {
-  const { isLoginLoading, loadingMessage } = useIsLoginStore();
+// const GlobalLoader = () => {
+//   const { isLoginLoading, loadingMessage } = useIsLoginStore();
 
-  if (!isLoginLoading) return null;
+//   if (!isLoginLoading) return null;
 
-  return (
-    <div
-      style={{
-        position: "fixed",
-        inset: 0,
-        backgroundColor: "rgba(255, 255, 255, 0.7)",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        zIndex: 9999,
-      }}
-    >
-      {/* ✅ only tip, remove children */}
-      <Spin size="large" tip={loadingMessage} />
-    </div>
-  );
-};
+//   return (
+//     <div
+//       style={{
+//         position: "fixed",
+//         inset: 0,
+//         backgroundColor: "rgba(255, 255, 255, 0.7)",
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         zIndex: 9999,
+//       }}
+//     >
+//       {/* ✅ only tip, remove children */}
+//       <Spin size="large" tip={loadingMessage} />
+//     </div>
+//   );
+// };
 
-export default GlobalLoader;
+// export default GlobalLoader;
 
