b6a5a9ce77cc2ec693b2249e9a5166cc7777b0e0
ShubhamChouhan97 <shubhamsc9799@gmail.com>
2026-03-30 20:27:23 +0530

    dddd

--- afrontend/src/Components/GlobalAlert/index.tsx
+++ afrontend/src/Components/GlobalAlert/index.tsx
@@ -9,7 +9,7 @@ const GlobalAlert = () => {
     if (message && type) {
       const timer = setTimeout(() => {
         clearAlert();
-      }, 3000); // auto close after 3s
+      }, 3000); // auto close after 3s ddd
 
       return () => clearTimeout(timer);
     }
